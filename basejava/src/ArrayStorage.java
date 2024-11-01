500de8700a7e2088c62755f31c7c0a12895673ba
tttenko <tttenko37@gmail.com>
2024-11-01 17:16:29 +0300

    Add files via upload

---
+++ basejava/src/ArrayStorage.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    Resume[] storage = new Resume[10000];
+    private int size = 0;
+
+    void clear() {
+        Arrays.fill(storage, 0, size, null);
+    }
+
+    void save(Resume r) {
+        if (size >= storage.length) {
+            System.out.println("Места для резюме больше нет.");
+        }
+
+        if (get(r.uuid) != null) {
+            System.out.println("Такое резюме уже есть.");
+        }
+
+        storage[size] = r;
+        size++;
+    }
+
+
+    Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
+        return null;
+    }
+
+    void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+                return;
+            }
+        }
+        System.out.println("Такое резюме не найдено.");
+    }
+
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    int size() {
+        return size;
+    }
+}
